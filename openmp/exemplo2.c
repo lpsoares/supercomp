838f59f8ce8367a7cf9319d770d3390635e6567b
Luciano Pereira Soares <lpsoares@gmail.com>
2018-02-17 21:58:04 -0200

    ajustes no exemplo2

--- openmp/exemplo2.c
+++ openmp/exemplo2.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <omp.h>
 int main() {
+	int acum=0;
 	#pragma omp parallel
 	{
 		int n = omp_get_num_threads(); // armazenará o numero de thread
 		int t = omp_get_thread_num(); // armazenará o identificador da thread
-		printf("Processando na thread %d num total de %d threads\n", t, n);
+		acum+=1;
+		printf("Thread %d/%d, acumulando %d\n", t, n, acum);
 	}
 	return 0;
 }
