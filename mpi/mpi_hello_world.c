e237f98b3da4cb89752d6d0cb8b9cb8a97081da5
Luciano Pereira Soares <lpsoares@gmail.com>
2018-02-19 21:58:20 -0300

    iniciando arquivos para o MPI

---
+++ mpi/mpi_hello_world.c
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main() {
+  int processes;
+  int size;
+  int rank;
+  char processor[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+
+  MPI_Init(NULL, NULL);	//inicia o MPI
+
+  MPI_Comm_size(MPI_COMM_WORLD, &size); // recupera número de processos
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank); // recupera o indice(rank) do processo
+
+  MPI_Get_processor_name(processor, &name_len); // recuper nome do processor
+
+  printf("host: %s (rank %d de %d)\n", processor, rank, size);
+
+  MPI_Finalize(); // encerra o MPI
+}
